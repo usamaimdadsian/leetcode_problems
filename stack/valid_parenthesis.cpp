44e4a347724d5187fe3e36fc9c6e723fd439ab63
Usama @ Horse <usamaimdadsian@gmail.com>
2024-01-30 00:06:29 +0500

    valid parenthesis

--- stack/valid_parenthesis.cpp
+++ stack/valid_parenthesis.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
@@ -13,4 +15,41 @@ An input string is valid if:
     Every close bracket has a corresponding open bracket of the same type.
 */
 
-int main() { cout << "Hello World!"; }
+bool is_valid_v1(string s) {
+  vector<char> stack;
+  unordered_map<char, char> m = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
+  for (char ts : s) {
+    char last_char;
+    if (stack.size() > 0) {
+      last_char = stack[stack.size() - 1];
+      // cout << "\n last stack char: " << last_char << " current char: " << ts
+      // << endl;
+      // cout << m.find(ts)->first << "HELLO" << m.find(ts)->second << endl;
+      if (m[last_char] == ts)
+        stack.pop_back();
+      else if (m.find(ts) == m.end())
+        return false;
+      else
+        stack.push_back(ts);
+    } else
+      stack.push_back(ts);
+  }
+
+  return (stack.size() > 0 ? false : true);
+}
+
+void test(string s) { cout << s << ": " << is_valid_v1(s) << endl; }
+
+int main() {
+  string s1 = "()[]{}";
+  test(s1);
+
+  string s2 = "()";
+  test(s2);
+
+  string s3 = "}()";
+  test(s3);
+
+  string s4 = "({})";
+  test(s4);
+}
